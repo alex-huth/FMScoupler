391acab0e966458c9b6f01cb5e182c0f48208428
alex-huth <alexander.huth@noaa.gov>
2025-02-24 13:24:46 -0500

    debugging

--- full/coupler_main.F90
+++ full/coupler_main.F90
@@ -658,7 +658,7 @@ program coupler_main
       if (combined_ice_and_ocean) then
         call flux_ice_to_ocean_stocks(Ice)
         call update_slow_ice_and_ocean(ice_ocean_driver_CS, Ice, Ocean_state, Ocean, &
-                                       Ice_ocean_boundary, Time_ocean, Time_step_cpld )
+                                       Ice_ocean_boundary, Time_ocean, Time_step_cpld, OIB=Ocean_ice_boundary)
       else
         if (do_chksum) call coupler_chksum_obj%get_ocean_chksums('update_ocean_model-', nc)
         ! update_ocean_model since fluxes don't change here
